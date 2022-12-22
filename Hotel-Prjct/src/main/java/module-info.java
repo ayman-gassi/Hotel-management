ffe5407d1eda233bf0ca203a208e861fc84951f9
ayman <itsmeaymane2003@gmail.com>
2022-12-22 23:13:01 +0100

    delete

--- Hotel-Prjct/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.hotelprjct {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.hotelprjct to javafx.fxml;
-    exports com.example.hotelprjct;
-}

0868c2ec9ec975262ba874e7e2d7f230ae1775dd
Zaid <zd.mahsoune@gmail.com>
2022-12-24 23:59:02 +0100

    ADD DATABASE AND TABLES CREATION FILE

---
+++ DataBase/CREATE DB-CREATE TABLES.sql
@@ -0,0 +1,65 @@
+CREATE DATABASE HOTELAPP;
+
+USE HOTELAPP;
+
+CREATE TABLE Room (
+	roomNo INT,
+    roomType VARCHAR(20),
+    roomPrice FLOAT,
+    occupancy INT
+);
+
+CREATE TABLE Booking (
+	bookingId INT,
+    clientId INT,
+    roomNo INT,
+    arrivalDate DATE, 
+    departureDate DATE,
+    numAdult INT,
+    numChildren INT
+);
+
+CREATE TABLE Bill (
+	invoiceNo INT,
+    bookingId INT,
+    clientId INT,
+	paymentDate DATE,
+    paymentMode VARCHAR(10),
+    payedCharges FLOAT,
+    creditCardNo INT,
+    expireDate VARCHAR(5)  
+); 
+
+CREATE TABLE Client (
+	clientId INT,
+    bookingId INT,
+    clientTitle VARCHAR(20),
+    clientTitleNo VARCHAR(50),
+    firstName VARCHAR(50),
+    lastName VARCHAR(50),
+    DOB DATE,
+    gender CHAR(1),
+    phoneNo INT,
+    email VARCHAR(50),
+    pwd VARCHAR(100),
+    adresse VARCHAR(50),
+    city VARCHAR(50),
+    country VARCHAR(50)
+);
+
+CREATE TABLE Admin (
+	AdminId INT,
+    AdminTitle VARCHAR(20),
+    AdimnTitleNo VARCHAR(50),
+    firstName VARCHAR(50),
+    lastName VARCHAR(50),
+    DOB DATE,
+    gender CHAR(1),
+    phoneNo INT,
+    email VARCHAR(50),
+    pwd VARCHAR(100),
+    adresse VARCHAR(50),
+    city VARCHAR(50),
+    country VARCHAR(50),
+    salary FLOAT
+);

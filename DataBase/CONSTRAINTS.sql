524a4f07bb071289eb11a17c57ad1fa6da1e89d3
Zaid <zd.mahsoune@gmail.com>
2022-12-25 01:08:10 +0100

    ADD TABLES CONSTRAINTS FILE

---
+++ DataBase/CONSTRAINTS.sql
@@ -0,0 +1,104 @@
+USE HOTELAPP;
+
+ALTER TABLE booking
+ADD COLUMN invoiceno INT;
+
+ALTER TABLE admin
+CHANGE AdimnTitleNo admintitleno INT;
+
+# PRIMARY KEY CONSTRAINT
+
+ALTER TABLE admin
+ADD CONSTRAINT admin_pk
+PRIMARY KEY(adminid); 
+
+ALTER TABLE bill
+ADD CONSTRAINT bill_pk
+PRIMARY KEY(invoiceno); 
+
+ALTER TABLE booking
+ADD CONSTRAINT booking_pk
+PRIMARY KEY(bookingid);
+
+ALTER TABLE client
+ADD CONSTRAINT client_pk
+PRIMARY KEY(clientid);
+
+ALTER TABLE room
+ADD CONSTRAINT room_pk
+PRIMARY KEY(roomno);
+
+# FOREIGN KEY CONSTRAINT
+
+# BILL
+
+ALTER TABLE bill
+ADD CONSTRAINT bill_booking_fk
+FOREIGN KEY(bookingid)
+REFERENCES booking(bookingid);
+
+ALTER TABLE bill
+ADD CONSTRAINT bill_client_fk
+FOREIGN KEY(clientid)
+REFERENCES client(clientid);
+
+# BOOKING 
+
+ALTER TABLE booking
+ADD CONSTRAINT booking_client_fk
+FOREIGN KEY(clientid)
+REFERENCES client(clientid);
+
+ALTER TABLE booking
+ADD CONSTRAINT booking_room_fk
+FOREIGN KEY(roomno)
+REFERENCES room(roomno);
+
+ALTER TABLE booking
+ADD CONSTRAINT booking_bill_fk
+FOREIGN KEY(invoiceno)
+REFERENCES bill(invoiceno);
+
+
+# CLIENT
+
+ALTER TABLE client
+ADD CONSTRAINT client_booking_fk
+FOREIGN KEY(bookingid)
+REFERENCES booking(bookingid);
+
+# UNIQUE CONSTRAINT
+
+# CLIENT
+
+ALTER TABLE client
+ADD CONSTRAINT client_email_uk
+UNIQUE(email);
+
+ALTER TABLE client
+ADD CONSTRAINT client_phoneno_uk
+UNIQUE(phoneno);
+
+ALTER TABLE client
+ADD CONSTRAINT client_title_id_uk
+UNIQUE(clienttitleno);
+
+# BILL 
+
+ALTER TABLE bill
+ADD CONSTRAINT bill_credit_no_uk
+UNIQUE(creditcardno);
+
+# ADMIN
+
+ALTER TABLE admin
+ADD CONSTRAINT admin_email_uk
+UNIQUE(email);
+
+ALTER TABLE admin
+ADD CONSTRAINT admin_phoneno_uk
+UNIQUE(phoneno);
+
+ALTER TABLE admin
+ADD CONSTRAINT admin_title_id_uk
+UNIQUE(admintitleno);

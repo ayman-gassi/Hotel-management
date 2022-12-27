e263b7ff9b3c67b17658b88dcc7617411b23bffe
ayman <itsmeaymane2003@gmail.com>
2022-12-27 16:02:40 +0100

    HOME PAGE (CLIENT && ADMIN)

--- src/main/java/com/example/demo1/HelloApplication.java
+++ src/main/java/com/example/demo1/HelloApplication.java
@@ -2,7 +2,10 @@ package com.example.demo1;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,15 +13,22 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 500, 500);
-        stage.setTitle("Hello!");
+
+
+        stage.setTitle("Hotel | HOME");
+        Parent group = FXMLLoader.load(getClass().getResource("hello-view.fxml"));
+        Scene scene = new Scene(group);
         stage.setScene(scene);
         stage.show();
+        stage.setWidth(912);
+        stage.setHeight(520);
+        Image icon = new Image("file:src/main/resources/images/hotel.png");
+        stage.getIcons().add(icon);
         stage.setResizable(false);
+
+
     }
 
     public static void main(String[] args) {
         launch();
-    }
-}
+}}
--- src/main/java/com/example/demo1/HelloController.java
+++ src/main/java/com/example/demo1/HelloController.java
@@ -1,14 +1,48 @@
 package com.example.demo1;
 
+import javafx.event.ActionEvent;
+import javafx.event.Event;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
+    private Stage stage;
+    private Scene scene;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+
+    public void switchtobooking(ActionEvent event) throws IOException {
+        Parent group = FXMLLoader.load(getClass().getResource("booking.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(group);
+        stage.setScene(scene);
+        stage.setWidth(912);
+        stage.setHeight(520);
+        stage.setTitle("Hotel | BOOKING");
+        stage.show();
+
     }
+    public void switchtohome(ActionEvent event) throws IOException {
+        Parent group = FXMLLoader.load(getClass().getResource("hello-view.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(group);
+        stage.setScene(scene);
+        stage.setWidth(912);
+        stage.setHeight(520);
+        stage.setTitle("Hotel | HOME");
+        stage.show();
+    }
+
+
+
+
 }
+

a25ced8434e53ba54991a333bd6eb26a4b2e2fdc
Zaid <zd.mahsoune@gmail.com>
2022-12-28 11:20:15 +0100

    SOLVE CONTROLLER PROBLEM

---
+++ src/main/java/com/example/demo1/NewControlller.java
@@ -0,0 +1,46 @@
+package com.example.demo1;
+
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class NewControlller implements Initializable {
+
+    private Stage stage;
+    private Scene scene;
+    @FXML
+    Button btnShow;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+    public void switchtoinfo(ActionEvent event){
+        Parent group = null;
+        try {
+            group = FXMLLoader.load(getClass().getResource("roominfo.fxml"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(group);
+        stage.setScene(scene);
+        stage.setWidth(912);
+        stage.setHeight(520);
+        stage.setTitle("Hotel | HOME");
+        stage.show();
+    }
+}

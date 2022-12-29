a11c449a378c9dc6462642efa4b879750299848c
Zakaria <zakariamachmach03@gmail.com>
2022-12-29 11:39:46 +0100

    User Account V3

--- src/main/java/com/example/demo1/UserAccount.java
+++ src/main/java/com/example/demo1/UserAccount.java
@@ -1,6 +1,7 @@
 package com.example.demo1;
 
 import com.example.demo1.DBConnect.DBConnectivity;
+import com.example.demo1.Modules.Client;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.event.Event;
@@ -15,9 +16,15 @@ import javafx.scene.text.Text;
 
 import java.io.IOException;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
+
+import static javafx.css.StyleOrigin.USER;
 
 public class UserAccount implements Initializable {
     @FXML
@@ -59,18 +66,42 @@ public class UserAccount implements Initializable {
         Country.setEditable(false);
         City.setEditable(false);
         Salary.setEditable(false);
+        //Client C1 = new Client(2,"CNI","W1ew2","SAID","DDD","www@wwww.ma");
+         Client C2 = new Client();
+        Fullname.setText(C2.getFirstName());
+        Email.setText(C2.getEmail());
+//        Gender.set(C2.getGender());
+        Phone.setText(String.valueOf(C2.getPhoneNo()));
+//        DOB.setTex();
+        Password.setText(C2.getPwd());
+        NumId.setText(String.valueOf(C2.getClientID()));
+        Adress.setText(C2.getAdresse());
+        Title.setText(C2.getClientTitle());
+        TitleNum.setText(C2.getClientTitleNo());
+        Country.setText(C2.getCountry());
+        City.setText(C2.getCity());
+        Salary.setText("a");
 
 
-//       try{
-//            Client client = new Client();
-//            admin Admin = new admin();
-//            boolean rst = client.CheckUser(username.getText(),password.getText());
-//        }catch(Exception e){
-//            System.err.println(e.getMessage());
-//        }
+
+
 
     }
     public void editall(ActionEvent e){
+        Fullname.setStyle("-fx-border-color: gray;");
+        Email.setStyle("-fx-border-color: gray;");
+        Gender.setStyle("-fx-border-color: gray;");
+        Phone.setStyle("-fx-border-color: gray;");
+        DOB.setStyle("-fx-border-color: gray;");
+        Password.setStyle("-fx-border-color: gray;");
+        NumId.setStyle("-fx-border-color: gray;");
+        Adress.setStyle("-fx-border-color: gray;");
+        Title.setStyle("-fx-border-color: gray;");
+        TitleNum.setStyle("-fx-border-color: gray;");
+        Country.setStyle("-fx-border-color: gray;");
+        City.setStyle("-fx-border-color: gray;");
+        Salary.setStyle("-fx-border-color: gray;");
+
         Fullname.setEditable(true);
         Email.setEditable(true);
         Gender.setEditable(true);
@@ -97,7 +128,19 @@ public class UserAccount implements Initializable {
         }
     }
     public void SaveChanges(ActionEvent e){
-        System.out.println("Succesfully");
+        Fullname.setStyle("-fx-background-color: white;");
+        Email.setStyle("-fx-background-color: white;");
+        Gender.setStyle("-fx-background-color: white;");
+        Phone.setStyle("-fx-background-color: white;");
+        DOB.setStyle("-fx-background-color: white;");
+        Password.setStyle("-fx-background-color: white;");
+        NumId.setStyle("-fx-background-color: white;");
+        Adress.setStyle("-fx-background-color: white;");
+        Title.setStyle("-fx-background-color: white;");
+        TitleNum.setStyle("-fx-background-color: white;");
+        Country.setStyle("-fx-background-color: white;");
+        City.setStyle("-fx-background-color: white;");
+        Salary.setStyle("-fx-background-color: white;");
 
 
 
